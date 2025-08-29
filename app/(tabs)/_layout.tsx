326ae4025494902c899d3668c0691a0b26b234ef
giakhalil <gia.khalil@stud.uniroma3.it>
2025-08-29 22:02:45 +0200

    create schermate con tabs, layout e dettaglio ricette
    - Creata cartella tabs con tre schermate: Home, Preferiti, Profilo
    - Implementata navigazione a tab con icone
    - Aggiunto layout principale con Stack per gestire tabs e dettaglio ricette
    - Creata pagina RicetteDetails
    - Aggiornata la grafica generale

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,125 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const _Layout = () => {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 90, 
+          paddingBottom: 15, 
+          paddingTop: 10, 
+          backgroundColor: '#A7c957',
+          borderTopWidth: 1,
+          borderTopColor: '#A7c957',
+        },
+      }}
+    >
+      <Tabs.Screen 
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => ( 
+            <View style={{ 
+              alignItems: 'center', 
+              justifyContent: 'center',
+              width: 90, 
+              height: 70, 
+            }}>
+              <Image 
+                source={require('@/assets/icons/home.png')} 
+                style={{ 
+                  width: 25, 
+                  height: 25, 
+                  tintColor: focused ? '#bc4749' : '#386641',
+                  marginBottom: 4, 
+                }}
+              />
+              <Text style={{ 
+                color: focused ? '#bc4749' : '#386641', 
+                fontSize: 13, 
+                textAlign: 'center',
+                width: '100%',
+              }}>
+                Home
+              </Text>
+            </View>
+          )
         }}
       />
-      <Tabs.Screen
-        name="explore"
+
+      <Tabs.Screen 
+        name="saved"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => ( 
+            <View style={{ 
+              alignItems: 'center', 
+              justifyContent: 'center',
+              width: 90,
+              height: 70,
+            }}>
+              <Image 
+                source={require('@/assets/icons/saved.png')} 
+                style={{ 
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#bc4749' : '#386641',
+                  marginBottom: 4,
+                }}
+              />
+              <Text style={{ 
+                color: focused ? '#bc4749' : '#386641', 
+                fontSize: 13,
+                textAlign: 'center',
+                width: '100%',
+              }}>
+                Ricette
+              </Text>
+            </View>
+          )
+        }}
+      />
+
+      <Tabs.Screen 
+        name="profile"
+        options={{
+          title: "",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => ( 
+            <View style={{ 
+              alignItems: 'center', 
+              justifyContent: 'center',
+              width: 90,
+              height: 70,
+            }}>
+              <Image 
+                source={require('@/assets/icons/profile.png')} 
+                style={{ 
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#bc4749' : '#386641',
+                  marginBottom: 4,
+                }}
+              />
+              <Text style={{ 
+                color: focused ? '#bc4749' : '#386641', 
+                fontSize: 13,
+                textAlign: 'center',
+                width: '100%',
+              }}>
+                Profilo
+              </Text>
+            </View>
+          )
         }}
       />
     </Tabs>
   );
 }
+
+export default _Layout;
---
+++ app/ricette/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const RicetteDetails = () => {
+  return (
+    <View>
+      <Text>Ricette</Text>
+    </View>
+  );
+}
+
+export default RicetteDetails
